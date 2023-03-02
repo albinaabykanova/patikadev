d99f9f6b163e4077ede4f2bc50d5d740a42f853e
albinaabykanova <abykanovaalbina@gmail.com>
2023-03-02 16:25:01 +0300

    js bolumu devam ediyor

---
+++ js/06-veri-turu-ile.js
@@ -0,0 +1,19 @@
+
+let price = 111
+let stringPrice = "111"
+let hasPassword = true
+
+consıle.log(
+     "price: ",
+     typeof(price)
+)
+
+consıle.log(
+    "Stringprice: ",
+    typeof(stringPrice)
+)
+
+consıle.log(
+    "hasPassword: ",
+    typeof(hasPassword)
+)
